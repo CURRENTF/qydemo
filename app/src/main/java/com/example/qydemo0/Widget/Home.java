22c1a090338a48b8728df5c69a5314a628eff11d
sushe <1206013760@qq.com>
2021-04-08 05:54:46 +0800

    3

---
+++ app/src/main/java/com/example/qydemo0/Widget/Home.java
@@ -0,0 +1,200 @@
+package com.example.qydemo0.Widget;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.example.qydemo0.FollowerAndFanActivity;
+import com.example.qydemo0.LearningListActivity;
+import com.example.qydemo0.PlayerActivity;
+import com.example.qydemo0.QYAdapter.ImageNetAdapter;
+import com.example.qydemo0.QYpack.Constant;
+import com.example.qydemo0.QYpack.GlobalVariable;
+import com.example.qydemo0.QYpack.Img;
+import com.example.qydemo0.QYpack.Json2X;
+import com.example.qydemo0.QYpack.MsgProcess;
+import com.example.qydemo0.QYpack.QYrequest;
+import com.example.qydemo0.QYpack.ShowProgressDialog;
+import com.example.qydemo0.QYpack.TimeTool;
+import com.example.qydemo0.R;
+import com.example.qydemo0.SearchActivity;
+import com.example.qydemo0.UploadActivity;
+import com.example.qydemo0.UserSettingActivity;
+import com.example.qydemo0.bean.DataBean;
+import com.example.qydemo0.ui.dashboard.DashboardFragment;
+import com.example.qydemo0.ui.home.HomeFragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.youth.banner.Banner;
+import com.youth.banner.indicator.CircleIndicator;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class Home extends RelativeLayout implements View.OnClickListener {
+
+    private Activity context;
+    private View mView;
+
+    public LinearLayout scrollViewForVideos = null;
+    public QYScrollView scrollView = null;
+    Unbinder bind;
+    public int startPos = 0, len = 20;
+    TimeTool timeTool = new TimeTool();
+
+    @BindView(R.id.banner_ad)
+    Banner banner_ad;
+
+    private Activity getActivity(){
+        return context;
+    }
+
+    public Home(@NonNull Context context) {
+        super(context);
+        this.context = (Activity) context;
+        init();
+    }
+
+    public Home(Context context, AttributeSet attrs){
+        super(context, attrs);
+        this.context = (Activity) context;
+        init();
+    }
+
+    void init(){
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mView = inflater.inflate(R.layout.fragment_home, this, true);
+
+        bind = ButterKnife.bind(this, mView);
+        //自定义的图片适配器，也可以使用默认的BannerImageAdapter
+        ImageNetAdapter adapter = new ImageNetAdapter(DataBean.getTestData3());
+
+        banner_ad.setAdapter(adapter)
+                .addBannerLifecycleObserver((AppCompatActivity)getActivity())//添加生命周期观察者
+                .setIndicator(new CircleIndicator(getActivity()))//设置指示器
+                .setOnBannerListener((data, position) -> {
+                    Snackbar.make(banner_ad, ((DataBean) data).title, Snackbar.LENGTH_SHORT).show();
+                });
+        scrollViewForVideos = mView.findViewById(R.id.home_scroll_for_video_cover);
+        scrollView = mView.findViewById(R.id.scroll_home);
+        GetUserRecommendation getUserRecommendation = new GetUserRecommendation();
+        getUserRecommendation.execute();
+
+        scrollView.setScanScrollChangedListener(new QYScrollView.ISmartScrollChangedListener() {
+            @Override
+            public void onScrolledToBottom() {
+                if(!timeTool.checkFreq()) return;
+                GetUserRecommendation getUserRecommendation = new GetUserRecommendation();
+                getUserRecommendation.execute();
+                Log.d("hjt.scroll.bottom", "true");
+                Log.d("hjt", "已添加");
+            }
+
+            @Override
+            public void onScrolledToTop() {
+                Log.d("hjt.scroll.top", "true");
+            }
+        });
+
+        FloatingActionButton fbtn = mView.findViewById(R.id.button_add_my_video);
+        fbtn.setOnClickListener(this);
+        LinearLayout txt = mView.findViewById(R.id.button_search);
+        txt.setOnClickListener(this);
+        Button btn = mView.findViewById(R.id.button_image_learn_list);
+        btn.setOnClickListener(this);
+    }
+
+    void unbind(){
+        bind.unbind();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_add_my_video:
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), UploadActivity.class);
+                getActivity().startActivity(intent);
+                break;
+
+            case R.id.button_search:
+                Intent intent2 = new Intent();
+                intent2.setClass(getActivity(), SearchActivity.class);
+                getActivity().startActivity(intent2);
+                break;
+            case R.id.button_image_learn_list:
+                Intent intent3 = new Intent();
+                intent3.setClass(getActivity(), LearningListActivity.class);
+                getActivity().startActivity(intent3);
+                break;
+        }
+    }
+
+    class GetUserRecommendation extends AsyncTask<String, Integer, JSONArray> {
+
+        @Override
+        protected JSONArray doInBackground(String... strings) {
+            QYrequest htp = new QYrequest();
+            Log.d("hjt.recommendation.info", String.valueOf(startPos) + "," + String.valueOf(len));
+            return MsgProcess.msgProcessArr(htp.advanceGet(Constant.mInstance.user_recommendation_url +
+                            Json2X.Json2StringGet("start", String.valueOf(startPos), "lens", String.valueOf(len)),
+                    "Authorization", GlobalVariable.mInstance.token), false);
+        }
+
+        @Override
+        protected void onPostExecute(JSONArray jsonArray) {
+            startPos += len;
+            if(jsonArray == null){
+                Log.d("hjt.get.user.recommendation.fail", "null");
+            }
+            else {
+                for(int i = 0; i < jsonArray.length(); i++){
+                    WorkItem w = new WorkItem(getActivity());
+                    try {
+                        JSONObject j = (JSONObject) jsonArray.get(i);
+                        JSONObject coverInfo = j.getJSONObject("cover");
+                        w.init(coverInfo.getString("url"), j.getString("name"), j.getInt("like_num"),
+                                j.getInt("play_num"), j.getString("introduction"));
+                        w.id = j.getInt("id");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        return;
+                    }
+                    scrollViewForVideos.addView(w);
+                    w.setOnClickListener(new SendWorkId());
+                    scrollViewForVideos.addView(Img.linearLayoutDivideLine(getActivity()));
+                }
+            }
+        }
+    }
+
+    class SendWorkId implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            intent.setClass(getActivity(), PlayerActivity.class);
+            intent.putExtra("id", ((WorkItem)v).id);
+            getActivity().startActivity(intent);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/qydemo0/Widget/Post.java
@@ -0,0 +1,267 @@
+package com.example.qydemo0.Widget;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.example.qydemo0.FollowerAndFanActivity;
+import com.example.qydemo0.LearningListActivity;
+import com.example.qydemo0.PlayerActivity;
+import com.example.qydemo0.QYAdapter.ImageNetAdapter;
+import com.example.qydemo0.QYpack.Constant;
+import com.example.qydemo0.QYpack.GlobalVariable;
+import com.example.qydemo0.QYpack.Img;
+import com.example.qydemo0.QYpack.Json2X;
+import com.example.qydemo0.QYpack.MsgProcess;
+import com.example.qydemo0.QYpack.QYrequest;
+import com.example.qydemo0.QYpack.ShowProgressDialog;
+import com.example.qydemo0.QYpack.TimeTool;
+import com.example.qydemo0.R;
+import com.example.qydemo0.SearchActivity;
+import com.example.qydemo0.UploadActivity;
+import com.example.qydemo0.UploadPostActivity;
+import com.example.qydemo0.UserSettingActivity;
+import com.example.qydemo0.bean.DataBean;
+import com.example.qydemo0.ui.dashboard.DashboardFragment;
+import com.example.qydemo0.ui.home.HomeFragment;
+import com.example.qydemo0.ui.posts.PostsFragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.youth.banner.Banner;
+import com.youth.banner.indicator.CircleIndicator;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class Post extends RelativeLayout implements View.OnClickListener {
+
+    private Activity context;
+    private View mView;
+
+    int[] buttons = {R.id.add_post, R.id.button_post_recommendation, R.id.button_post_follow};
+    RelativeLayout rc_layout, f_layout;
+    QYScrollView rc_view, f_view;
+    int rc_startPos = 0, rc_len = 20;
+    int switcher = 0;
+    // 0 rc 1 f
+    TimeTool timeTool = new TimeTool();
+
+
+    private Activity getActivity(){
+        return context;
+    }
+
+    public Post(@NonNull Context context) {
+        super(context);
+        this.context = (Activity) context;
+        init();
+    }
+
+    public Post(Context context, AttributeSet attrs){
+        super(context, attrs);
+        this.context = (Activity) context;
+        init();
+    }
+
+    void init() {
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mView = inflater.inflate(R.layout.fragment_posts, this, true);
+
+        for(int i = 0; i < buttons.length; i++){
+            View btn = mView.findViewById(buttons[i]);
+            btn.setOnClickListener(this);
+        }
+
+        rc_layout = mView.findViewById(R.id.rela_layout_posts_recommendation);
+        f_layout = mView.findViewById(R.id.rela_layout_posts_follow);
+        rc_view = mView.findViewById(R.id.view_posts_recommendation);
+        f_view = mView.findViewById(R.id.view_posts_follow);
+        rc_view.setScanScrollChangedListener(new QYScrollView.ISmartScrollChangedListener() {
+            @Override
+            public void onScrolledToBottom() {
+                if(!timeTool.checkFreq()) return;
+                GetRecommendationPost getRecommendationPost = new GetRecommendationPost();
+                getRecommendationPost.execute();
+            }
+            @Override
+            public void onScrolledToTop() {
+                Log.d("hjt.scroll.top", "true");
+            }
+        });
+        f_view.setScanScrollChangedListener(new QYScrollView.ISmartScrollChangedListener() {
+            @Override
+            public void onScrolledToBottom() {
+                if(!timeTool.checkFreq()) return;
+                GetFollowedPost getFollowedPost = new GetFollowedPost();
+                getFollowedPost.execute();
+            }
+            @Override
+            public void onScrolledToTop() {
+                Log.d("hjt.scroll.top", "true");
+            }
+        });
+        GetRecommendationPost getRecommendationPost = new GetRecommendationPost();
+        getRecommendationPost.execute();
+        GetFollowedPost getFollowedPost = new GetFollowedPost();
+        getFollowedPost.execute();
+    }
+
+    @Override
+    public void onClick(View v) {
+        TextView t;
+        switch (v.getId()){
+            case R.id.add_post:
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), UploadPostActivity.class);
+                getActivity().startActivity(intent);
+                break;
+            case R.id.button_post_recommendation:
+                if(switcher == 0) break;
+                switcher = 0;
+                ((TextView)v).setTextColor(getActivity().getColor(R.color.red));
+                t = getActivity().findViewById(R.id.button_post_follow);
+                t.setTextColor(getActivity().getColor(R.color.black));
+                Animation animation = AnimationUtils.loadAnimation(getActivity(), R.anim.ani_right_translate_alpha_500ms);
+                f_view.startAnimation(animation);
+                Animation a2 = AnimationUtils.loadAnimation(getActivity(), R.anim.ani_right_translate_in_alpha_500ms);
+                rc_view.startAnimation(a2);
+                ChangeVisibility changeVisibility = new ChangeVisibility();
+                changeVisibility.execute(true);
+                break;
+            case R.id.button_post_follow:
+                if (switcher == 1) break;
+                switcher = 1;
+                ((TextView)v).setTextColor(getResources().getColor(R.color.red));
+                t = getActivity().findViewById(R.id.button_post_recommendation);
+                t.setTextColor(getResources().getColor(R.color.black));
+                Animation animation2 = AnimationUtils.loadAnimation(getActivity(), R.anim.ani_left_translate_alpha_500ms);
+                rc_view.startAnimation(animation2);
+                Animation a3 = AnimationUtils.loadAnimation(getActivity(), R.anim.ani_left_translate_in_alpha_500ms);
+                f_view.startAnimation(a3);
+                ChangeVisibility changeVisibility2 = new ChangeVisibility();
+                changeVisibility2.execute(false);
+                break;
+        }
+    }
+
+    int lastF_id = -1;
+    class GetFollowedPost extends AsyncTask<String, Integer, JSONArray>{
+
+        @Override
+        protected JSONArray doInBackground(String... strings) {
+            QYrequest htp = new QYrequest();
+            Log.d("hjt.get.followed.post", "1");
+            return MsgProcess.msgProcessArr(htp.advanceGet(Constant.mInstance.post_url + "0/", "Authorization", GlobalVariable.mInstance.token), false);
+        }
+
+        @Override
+        protected void onPostExecute(JSONArray jsonArray) {
+            if(jsonArray == null){
+                Log.d("hjt.get.follow.post", "null_json");
+                return;
+            }
+            for(int i = 0; i < jsonArray.length(); i++){
+                try {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    PostItem postItem = new PostItem(getActivity());
+                    postItem.init(jsonObject);
+                    RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,  ViewGroup.LayoutParams.WRAP_CONTENT);
+                    if(lastF_id != -1) layoutParams.addRule(RelativeLayout.BELOW, lastF_id);
+                    else layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+                    lastF_id = View.generateViewId();
+                    postItem.setId(lastF_id);
+                    postItem.setLayoutParams(layoutParams);
+                    f_layout.addView(postItem);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    return;
+                }
+            }
+        }
+    }
+
+    int lastRc_id = -1;
+    class GetRecommendationPost extends AsyncTask<String, Integer, JSONArray>{
+        @Override
+        protected JSONArray doInBackground(String... strings) {
+            QYrequest htp = new QYrequest();
+            return MsgProcess.msgProcessArr(
+                    htp.advanceGet(Constant.mInstance.post_recommendation_url + Json2X.Json2StringGet("start", String.valueOf(rc_startPos), "lens", String.valueOf(rc_len)),
+                            "Authorization", GlobalVariable.mInstance.token), false
+            );
+        }
+
+        @Override
+        protected void onPostExecute(JSONArray jsonArray) {
+            if(jsonArray == null){
+                Log.d("hjt.get.recommendation.post", "null_json");
+                return;
+            }
+            for(int i = 0; i < jsonArray.length(); i++){
+                try {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    PostItem postItem = new PostItem(getActivity());
+                    postItem.init(jsonObject);
+                    RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,  ViewGroup.LayoutParams.WRAP_CONTENT);
+                    if(lastRc_id != -1) layoutParams.addRule(RelativeLayout.BELOW, lastRc_id);
+                    else layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+                    lastRc_id = View.generateViewId();
+                    postItem.setId(lastRc_id);
+                    postItem.setLayoutParams(layoutParams);
+                    rc_layout.addView(postItem);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    return;
+                }
+            }
+        }
+    }
+
+    class ChangeVisibility extends AsyncTask<Boolean, Integer, Boolean>{
+
+        @Override
+        protected Boolean doInBackground(Boolean... booleans) {
+            try {
+                Thread.sleep(Constant.mInstance.ani_time);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return booleans[0];
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            if(aBoolean){
+                rc_view.setVisibility(View.VISIBLE);
+                f_view.setVisibility(View.GONE);
+            }
+            else {
+                rc_view.setVisibility(View.GONE);
+                f_view.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+}
